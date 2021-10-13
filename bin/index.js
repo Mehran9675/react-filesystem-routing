a0babbc7f5979889fa6ebc02e291643f363a8387
Mehran <Mehranzahir@outlook.com>
2021-10-13 16:39:40 +0330

    changed genPaths path

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const genPath = require("../dist/genPath");
+const genPath = require(__dirname + "/../dist/genPath");
 const { Command } = require("commander");
 const { version, name } = require("../package.json");
 
