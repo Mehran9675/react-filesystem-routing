a4cc833ce26fd1c08d3240a94b86301464012bd5
Mehran <Mehranzahir@outlook.com>
2021-10-13 15:09:45 +0330

    added react component and typescript and dist

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const genPath = require("./dis/genPath/index.js");
+const { Command } = require("commander");
+const { version, name } = require("./package.json");
+
+const program = new Command();
+
+program.storeOptionsAsProperties(name);
+
+program.version(version, "-v --version", "Output the current version");
+
+program.requiredOption("-p,--path <path>", "Path to your pages folder");
+program.parse(process.argv);
+
+genPath(program.path);
